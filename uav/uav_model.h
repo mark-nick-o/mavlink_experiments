7875ae1e4bcbdc5886b8b45ac53a23529d0fc573
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-12 12:44:01 +0000

    Update uav_model.h

--- uav/uav_model.h
+++ uav/uav_model.h
@@ -21,6 +21,20 @@ namespace domain
         float groundspeed() const;
         float climb() const;
 
+        std::uint32_t time_boot_ms() const;                                                         /*< [ms] Timestamp (time since system boot).*/
+        std::uint32_t firmware_version() const;                                                     /*<  Version of the camera firmware (v << 24 & 0xff = Dev, v << 16 & 0xff = Patch, v << 8 & 0xff = Minor, v & 0xff = Major)*/
+        std::float32_t focal_length() const;                                                        /*< [mm] Focal length*/
+        std::float32_t sensor_size_h() const;                                                       /*< [mm] Image sensor size horizontal*/
+        std::float32_t sensor_size_v() const;                                                       /*< [mm] Image sensor size vertical*/
+        std::uint32_t flags() const;                                                                /*<  Bitmap of camera capability flags.*/
+        std::uint16_t resolution_h() const;                                                         /*< [pix] Horizontal image resolution*/
+        std::uint16_t resolution_v() const;                                                         /*< [pix] Vertical image resolution*/
+        std::uint16_t cam_definition_version() const;                                               /*<  Camera definition version (iteration)*/
+        std::uint8_t* vendor_name() const;                                                          /*<  Name of the camera vendor*/
+        std::uint8_t* model_name() const;                                                           /*<  Name of the camera model*/
+        std::uint8_t lens_id() const;                                                               /*<  Reserved for a lens ID*/
+        std::char* cam_definition_uri() const;  
+
         QGeoCoordinate position() const;
         QGeoCoordinate homePosition() const;
 
@@ -38,6 +52,20 @@ namespace domain
         float m_airspeed;
         float m_climb;
 
+        std::uint32_t time_boot_ms;                                                         /*< [ms] Timestamp (time since system boot).*/
+        std::uint32_t firmware_version;                                                     /*<  Version of the camera firmware (v << 24 & 0xff = Dev, v << 16 & 0xff = Patch, v << 8 & 0xff = Minor, v & 0xff = Major)*/
+        std::float32_t focal_length;                                                        /*< [mm] Focal length*/
+        std::float32_t sensor_size_h;                                                       /*< [mm] Image sensor size horizontal*/
+        std::float32_t sensor_size_v;                                                       /*< [mm] Image sensor size vertical*/
+        std::uint32_t flags;                                                                /*<  Bitmap of camera capability flags.*/
+        std::uint16_t resolution_h;                                                         /*< [pix] Horizontal image resolution*/
+        std::uint16_t resolution_v;                                                         /*< [pix] Vertical image resolution*/
+        std::uint16_t cam_definition_version;                                               /*<  Camera definition version (iteration)*/
+        std::uint8_t vendor_name[32u];                                                      /*<  Name of the camera vendor*/
+        std::uint8_t model_name[32u];                                                       /*<  Name of the camera model*/
+        std::uint8_t lens_id;                                                               /*<  Reserved for a lens ID*/
+        std::char cam_definition_uri[140u];  
+ 
         QGeoCoordinate m_position;
         QGeoCoordinate m_homePosition;
     };
