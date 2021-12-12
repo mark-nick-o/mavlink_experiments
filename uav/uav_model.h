c183372def8e881b240246edb9307a31e5037ec3
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-12 13:04:38 +0000

    Update uav_model.h

--- uav/uav_model.h
+++ uav/uav_model.h
@@ -52,19 +52,19 @@ namespace domain
         float m_airspeed;
         float m_climb;
 
-        std::uint32_t time_boot_ms;                                                         /*< [ms] Timestamp (time since system boot).*/
-        std::uint32_t firmware_version;                                                     /*<  Version of the camera firmware (v << 24 & 0xff = Dev, v << 16 & 0xff = Patch, v << 8 & 0xff = Minor, v & 0xff = Major)*/
-        std::float32_t focal_length;                                                        /*< [mm] Focal length*/
-        std::float32_t sensor_size_h;                                                       /*< [mm] Image sensor size horizontal*/
-        std::float32_t sensor_size_v;                                                       /*< [mm] Image sensor size vertical*/
-        std::uint32_t flags;                                                                /*<  Bitmap of camera capability flags.*/
-        std::uint16_t resolution_h;                                                         /*< [pix] Horizontal image resolution*/
-        std::uint16_t resolution_v;                                                         /*< [pix] Vertical image resolution*/
-        std::uint16_t cam_definition_version;                                               /*<  Camera definition version (iteration)*/
-        std::uint8_t vendor_name[32u];                                                      /*<  Name of the camera vendor*/
-        std::uint8_t model_name[32u];                                                       /*<  Name of the camera model*/
-        std::uint8_t lens_id;                                                               /*<  Reserved for a lens ID*/
-        std::char cam_definition_uri[140u];  
+        std::uint32_t m_time_boot_ms;                                                         /*< [ms] Timestamp (time since system boot).*/
+        std::uint32_t m_firmware_version;                                                     /*<  Version of the camera firmware (v << 24 & 0xff = Dev, v << 16 & 0xff = Patch, v << 8 & 0xff = Minor, v & 0xff = Major)*/
+        std::float32_t m_focal_length;                                                        /*< [mm] Focal length*/
+        std::float32_t m_sensor_size_h;                                                       /*< [mm] Image sensor size horizontal*/
+        std::float32_t m_sensor_size_v;                                                       /*< [mm] Image sensor size vertical*/
+        std::uint32_t m_flags;                                                                /*<  Bitmap of camera capability flags.*/
+        std::uint16_t m_resolution_h;                                                         /*< [pix] Horizontal image resolution*/
+        std::uint16_t m_resolution_v;                                                         /*< [pix] Vertical image resolution*/
+        std::uint16_t m_cam_definition_version;                                               /*<  Camera definition version (iteration)*/
+        std::uint8_t m_vendor_name[32u];                                                      /*<  Name of the camera vendor*/
+        std::uint8_t m_model_name[32u];                                                       /*<  Name of the camera model*/
+        std::uint8_t m_lens_id;                                                               /*<  Reserved for a lens ID*/
+        std::char m_cam_definition_uri[140u];  
  
         QGeoCoordinate m_position;
         QGeoCoordinate m_homePosition;
