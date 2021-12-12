93063fdc028d85e7aa73d54326f0a644cef1872d
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-12 13:33:44 +0000

    Update uav_model.cpp

--- uav/uav_model.cpp
+++ uav/uav_model.cpp
@@ -35,6 +35,19 @@ UavModel::UavModel(QObject* parent):
     m_yaw(::randNum(0, 360)),
     m_airspeed(::randNum(150, 500) * 0.1),
     m_position(55.9837, 37.2088, 0)
+    m_time_boot_ms(65501)
+    m_firmware_version(1001)
+    m_focal_length(1.2)
+    m_sensor_size_h(1)
+    m_sensor_size_v(2)
+    m_flags(43)
+    m_resolution_h(4)
+    m_resolution_v(6)
+    m_cam_definition_version(8)
+    m_vendor_name("Sony")
+    m_model_name("alpha750")
+    m_lens_id(67)
+    m_cam_definition_uri("http://121.1.1.2/pics/1.jpg")
 {
     qsrand(QDateTime::currentMSecsSinceEpoch() / 1000);
     this->startTimer(::interval);
@@ -105,3 +118,70 @@ float UavModel::pitch() const
 {
     return m_pitch;
 }
+
+std::uint32_t time_boot_ms() const
+{
+    return m_time_boot_ms; 
+}                                                    /*< [ms] Timestamp (time since system boot).*/
+            
+std::uint32_t firmware_version() const
+{
+    return m_firmware_version;
+}
+                                                     /*<  Version of the camera firmware (v << 24 & 0xff = Dev, v << 16 & 0xff = Patch, v << 8 & 0xff = Minor, v & 0xff = Major)*/
+std::float32_t focal_length() const
+{
+    return m_focal_length;    
+}
+                                                       /*< [mm] Focal length*/
+std::float32_t sensor_size_h() const
+{
+    return m_sensor_size_h;        
+}
+                                                      /*< [mm] Image sensor size horizontal*/
+std::float32_t sensor_size_v() const
+{
+    return m_sensor_size_v;        
+}
+                                                      /*< [mm] Image sensor size vertical*/
+std::uint32_t flags() const
+{
+    return m_flags;     
+}
+                                                      /*<  Bitmap of camera capability flags.*/
+std::uint16_t resolution_h() const
+{
+    return m_resolution_h;        
+}
+                                                      /*< [pix] Horizontal image resolution*/
+std::uint16_t resolution_v() const
+{
+    return m_resolution_v;   
+}
+                                                     /*< [pix] Vertical image resolution*/
+std::uint16_t cam_definition_version() const
+{
+    return m_cam_definition_version;   
+}
+                                                     /*<  Camera definition version (iteration)*/
+std::uint8_t* vendor_name() const
+{
+    return m_vendor_name;  
+}
+                                                     /*<  Name of the camera vendor*/
+std::uint8_t* model_name() const
+{
+    return m_model_name;      
+}
+                                                    /*<  Name of the camera model*/
+std::uint8_t lens_id() const
+{
+    return m_lens_id;       
+}
+                                                   /*<  Reserved for a lens ID*/
+std::char* cam_definition_uri() const
+{
+    return m_cam_definition_uri;                  /*<  Camera definition URI (if any, otherwise only basic functions will be available). HTTP- (http://) and MAVLink FTP- (mavlinkftp://) formatted URIs are allowed (and both must be supported by any GCS that implements the Camera Protocol).*/
+}
+
+                                                   
