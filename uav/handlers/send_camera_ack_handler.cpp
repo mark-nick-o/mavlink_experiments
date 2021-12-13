3eab389daf5f0e871c195c7bb5916407a1e43000
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-13 12:10:14 +0000

    Update send_camera_ack_handler.cpp

--- uav/handlers/send_camera_ack_handler.cpp
+++ uav/handlers/send_camera_ack_handler.cpp
@@ -48,18 +48,37 @@ void SendCameraAckHandler::sendCmdACKMAVLinkMessage( std::uint8_t res, std::uint
 /*
    This is next the message the Camera will send to the GCS to accept this communication
 */
-void SendCameraAckHandler::cameraACKCameraInformationReqAccepted( std::uint8_t target_sys, std::uint8_t componentId )
+void SendCameraAckHandler::cameraACKCameraInformationReqAccepted( std::uint8_t target_sys, std::uint8_t componentId, std::uint8_t progrez )
 {
    //const std::int8_t CAMERA_INFORMATION = 259;
    //target_sys = MAV_TYPE_GCS;
-   SendCameraAckHandler::sendCmdACKMAVLinkMessage( MAV_RESULT_ACCEPTED, 100, MAV_CMD_REQUEST_MESSAGE, target_sys, CAMERA_INFORMATION, std::uint8_t componentId);
+   SendCameraAckHandler::sendCmdACKMAVLinkMessage( MAV_RESULT_ACCEPTED, progrez, MAV_CMD_REQUEST_MESSAGE, target_sys, CAMERA_INFORMATION, std::uint8_t componentId);
+}
+
+/*
+   This is next the message the Camera will send to the GCS to accept this communication
+*/
+void SendCameraAckHandler::cameraACKCameraInformationReqAlready( std::uint8_t target_sys, std::uint8_t componentId, std::uint8_t progrez )
+{
+   //const std::int8_t CAMERA_INFORMATION = 259;
+   //target_sys = MAV_TYPE_GCS;
+   SendCameraAckHandler::sendCmdACKMAVLinkMessage( MAV_RESULT_IN_PROGRESS, progrez, MAV_CMD_REQUEST_MESSAGE, target_sys, CAMERA_INFORMATION, std::uint8_t componentId);
 }
 
 /*
     This is the message the Camera Board shall send back to the GCS on receipt of the above message
 */
-void SendCameraAckHandle::timerEvent(QTimerEvent* event)
+void SendCameraAckHandler::timerEvent(QTimerEvent* event)
 {
     Q_UNUSED(event)
-    SendCameraAckHandler::cameraACKCameraInformationReqAccepted( m_communicator->systemId(), m_communicator->componentId() );
+    if ((m_substate == DO_SENDING_ACK) && (m_sendState != 0))
+    {
+        SendCameraAckHandler::cameraACKCameraInformationReqAlready( m_communicator->systemId(), m_communicator->componentId(), 50 );
+        m_substate == DO_SENDING_ACK;
+    }
+    else if ((m_substate == DO_SEND_ACK) && (m_sendState == SENS_CI))
+    {
+        SendCameraAckHandler::cameraACKCameraInformationReqAccepted( m_communicator->systemId(), m_communicator->componentId(), 0 );
+        m_substate == DO_SENDING_ACK;
+    }
 }
