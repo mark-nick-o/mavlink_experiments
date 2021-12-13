7725865b291c2578801185417d23bdc31ded698c
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-13 16:57:54 +0000

    Update heartbeat_handler.cpp

--- gcs/handlers/heartbeat_handler.cpp
+++ gcs/handlers/heartbeat_handler.cpp
@@ -14,7 +14,7 @@ HeartHandler::HeartHandler(MavLinkCommunicator* communicator,
     AbstractHandler(communicator),
     m_model(model)
 {
-    // so far we dont send it back so this is out this->startTimer(40); // 25 Hz
+    this->startTimer(40); // 25 Hz
 }
 
 void HeartHandler::processMessage(const mavlink_message_t& message)
@@ -27,4 +27,23 @@ void HeartHandler::processMessage(const mavlink_message_t& message)
 
     qDebug() << "pitch" << heart.mavlink_version;
     m_sendState = HEART_RCV;     
+}
+
+void SendHeartBeatHandler::timerEvent(QTimerEvent* event)
+{
+    Q_UNUSED(event)
+
+    // reset the heartbeat state
+    //
+    m_sendState = 0;     
+  
+    // we are mot sending heartbeats out of the gcs 
+    //
+    // mavlink_message_t message;
+    // mavlink_heartbeat_t heartbeat;
+    // heartbeat.type = 1;
+
+    // mavlink_msg_heartbeat_encode(m_communicator->systemId(), m_communicator->componentId(), &message, &heartbeat);
+
+    // m_communicator->sendMessageOnLastReceivedLink(message);
 }
