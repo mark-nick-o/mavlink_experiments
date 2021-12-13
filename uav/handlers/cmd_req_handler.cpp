63a150cb6f2c6e4b0563c0b62058fd5e4361ee00
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-13 11:27:55 +0000

    Create cmd_req_handler.cpp

---
+++ uav/handlers/cmd_req_handler.cpp
@@ -0,0 +1,37 @@
+#include "cmd_req_handler.h"
+
+// MAVLink
+#include <mavlink.h>
+
+// Qt
+#include <QtMath>
+#include <QDebug>
+
+using namespace domain;
+
+CmdReqHandler::CmdReqHandler(MavLinkCommunicator* communicator):
+    AbstractHandler(communicator)
+{}
+
+void CmdReqHandler::processMessage(const mavlink_message_t& message)
+{
+    if (message.msgid != MAV_CMD_REQUEST_MESSAGE || message.sysid == 0) return;
+
+    mavlink_cmd_request_t cmdReq;
+    mavlink_msg_cmd_request_decode(&message, &cmdReq);
+
+	 switch (cmdReq.param1)
+	 {
+		   case CAMERA_INFORMATION:
+       /* parse information regarding the camera  */
+		   m_substate = DO_SEND_ACK;
+		   m_sendState = SENS_CI;
+       break;
+
+       case STORAGE_INFORMATION:
+       /* parse information regarding the storage media */
+		   m_substate = DO_SEND_ACK;
+		   m_sendState = SENS_SI;
+       break;					
+	 }
+}
