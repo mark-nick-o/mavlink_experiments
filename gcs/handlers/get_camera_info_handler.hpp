ee931a4019a17b0b24d665cd373b4e6e2d5961c0
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-13 16:02:23 +0000

    Update get_camera_info_handler.hpp

--- gcs/handlers/get_camera_info_handler.hpp
+++ gcs/handlers/get_camera_info_handler.hpp
@@ -1,1 +1,25 @@
+#ifndef GET_CAM_INFO_HANDLER_H
+#define GET_CAM_INFO_HANDLER_H
 
+#include "abstract_handler.h"
+
+namespace domain
+{
+
+    class GcsModel;
+    
+    class getCamInfoHandler: public AbstractHandler
+    {
+    public:
+        getCamInfoHandler(MavLinkCommunicator* communicator, GcsModel* model);
+
+    public slots:
+        void processMessage(const mavlink_message_t& message) override;
+
+    protected:
+        void timerEvent(QTimerEvent *event) override;
+
+    private:
+        GcsModel* m_model;
+    };
+}
