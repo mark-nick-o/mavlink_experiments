7fecb0d172230becc600bd1a9f444c18fb786afe
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-13 11:40:10 +0000

    Update cmd_req_handler.cpp

--- uav/handlers/cmd_req_handler.cpp
+++ uav/handlers/cmd_req_handler.cpp
@@ -20,18 +20,18 @@ void CmdReqHandler::processMessage(const mavlink_message_t& message)
     mavlink_cmd_request_t cmdReq;
     mavlink_msg_cmd_request_decode(&message, &cmdReq);
 
-	 switch (cmdReq.param1)
-	 {
-		   case CAMERA_INFORMATION:
-       /* parse information regarding the camera  */
-		   m_substate = DO_SEND_ACK;
-		   m_sendState = SENS_CI;
-       break;
+    switch (cmdReq.param1)
+    {
+	   case CAMERA_INFORMATION:
+           /* parse information regarding the camera  */
+	   m_substate = DO_SEND_ACK;
+	   m_sendState = SENS_CI;
+           break;
 
-       case STORAGE_INFORMATION:
-       /* parse information regarding the storage media */
-		   m_substate = DO_SEND_ACK;
-		   m_sendState = SENS_SI;
-       break;					
-	 }
+           case STORAGE_INFORMATION:
+           /* parse information regarding the storage media */
+	   m_substate = DO_SEND_ACK;
+	   m_sendState = SENS_SI;
+           break;					
+    }
 }
