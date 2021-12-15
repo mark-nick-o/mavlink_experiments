7867f88971f056e54f4d716ddce359924b25694e
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-15 14:06:34 +0000

    Update camera_status_data.h

--- uav/handlers/camera_status_data.h
+++ uav/handlers/camera_status_data.h
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-// define the reject bits
+// define the reject and command execute failure bits
 //
 #define CI_BIT 1
 #define CS_BIT 2
@@ -17,12 +17,13 @@ using namespace std;
 #define CCS_BIT 8
 #define VS_BIT 16
 #define US_BIT 32
-#define CI_FAIL_BIT 64
-#define CS_FAIL_BIT 128
-#define SI_FAIL_BIT 256
-#define CCS_FAIL_BIT 512
-#define VS_FAIL_BIT 1024
-#define SPARE_BIT 2048
+#define FMT_BIT 64
+#define CI_FAIL_BIT 128
+#define CS_FAIL_BIT 256
+#define SI_FAIL_BIT 512
+#define CCS_FAIL_BIT 1024
+#define VS_FAIL_BIT 2048
+#define FORMAT_FAIL_BIT 4096
 
 struct cam_config_t
 {
@@ -61,11 +62,11 @@ struct vid_control_t
 
 struct cameraMountOrietation_t
 {
-  std::uint32_t time_boot_ms;		                // ms	Timestamp (time since system boot).
-  std::float roll;		                        // deg	Roll in global frame (set to NaN for invalid).
-  std::float pitch;		                        // deg	Pitch in global frame (set to NaN for invalid).
-  std::float yaw;		                        // deg	Yaw relative to vehicle (set to NaN for invalid).
-  std::float yaw_absolute; 		                // deg	Yaw in absolute frame relative to Earth's North, north is 0 (set to NaN for invalid).
+  std::uint32_t time_boot_ms;		               // ms	Timestamp (time since system boot).
+  std::float roll;		                         // deg	Roll in global frame (set to NaN for invalid).
+  std::float pitch;		                         // deg	Pitch in global frame (set to NaN for invalid).
+  std::float yaw;		                           // deg	Yaw relative to vehicle (set to NaN for invalid).
+  std::float yaw_absolute; 		                 // deg	Yaw in absolute frame relative to Earth's North, north is 0 (set to NaN for invalid).
   std::uint8_t reqfailure;
 };
 
@@ -73,42 +74,42 @@ struct cameraInformation_t
 {
    std::uint32_t time_boot_ms;		              // ms		Timestamp (time since system boot).
    std::uint8_t vendor_name[32];	              // Name of the camera vendor
-   std::uint8_t model_name[32];	                      // Name of the camera model
-   std::uint32_t firmware_version;	              // Version of the camera firmware, encoded as: (Dev & 0xff) << 24 | (Patch & 0xff) << 16 | (Minor & 0xff) << 8 | (Major & 0xff)
-   std::float focal_length;		              // mm		Focal length
-   std::float sensor_size_h;		              // mm		Image sensor size horizontal
-   std::float sensor_size_v;		              // mm		Image sensor size vertical
+   std::uint8_t model_name[32];	                // Name of the camera model
+   std::uint32_t firmware_version;	            // Version of the camera firmware, encoded as: (Dev & 0xff) << 24 | (Patch & 0xff) << 16 | (Minor & 0xff) << 8 | (Major & 0xff)
+   std::float focal_length;		                  // mm		Focal length
+   std::float sensor_size_h;		                // mm		Image sensor size horizontal
+   std::float sensor_size_v;		                // mm		Image sensor size vertical
    std::uint16_t resolution_h;		              // pix		Horizontal image resolution
    std::uint16_t resolution_v;		              // pix		Vertical image resolution
-   std::uint8_t lens_id;			      // Reserved for a lens ID
-   std::uint32_t flags;			              // CAMERA_CAP_FLAGS	Bitmap of camera capability flags.
+   std::uint8_t lens_id;			                  // Reserved for a lens ID
+   std::uint32_t flags;			                    // CAMERA_CAP_FLAGS	Bitmap of camera capability flags.
    std::uint16_t cam_definition_version;	      // Camera definition version (iteration)
-   std::char cam_definition_uri[140];		      // Camera definition URI (if any, otherwise only basic functions will be available). HTTP- (http://) and MAVLink FTP- (mavlinkftp://) formatted URIs are allowed (and both must be supported by any GCS that implements the Camera Protocol). The definition file may be xz compressed, which will be indicated by the file extension .xml.xz (a GCS that implements the protocol must support decompressing the file).
+   std::char cam_definition_uri[140];		        // Camera definition URI (if any, otherwise only basic functions will be available). HTTP- (http://) and MAVLink FTP- (mavlinkftp://) formatted URIs are allowed (and both must be supported by any GCS that implements the Camera Protocol). The definition file may be xz compressed, which will be indicated by the file extension .xml.xz (a GCS that implements the protocol must support decompressing the file).
    std::uint8_t reqfailure;
 };
 
 struct cameraImageSet_t
 {
-  std::uint32_t time_boot_ms;		           // ms Timestamp (time since system boot).
-  std::uint8_t mode_id;			           // CAMERA_MODE	Camera mode
-  std::float zoomLevel; 			   // Current zoom level (0.0 to 100.0, NaN if not known)
-  std::float focusLevel; 	                   // Current focus level (0.0 to 100.0, NaN if not known)
+  std::uint32_t time_boot_ms;		        // ms Timestamp (time since system boot).
+  std::uint8_t mode_id;			            // CAMERA_MODE	Camera mode
+  std::float zoomLevel; 			          // Current zoom level (0.0 to 100.0, NaN if not known)
+  std::float focusLevel; 	              // Current focus level (0.0 to 100.0, NaN if not known)
   std::uint8_t reqfailure;
 };
 
 struct storageInfo_t
 {
-   std::uint8_t storage_id;			    // Storage ID (1 for first, 2 for second, etc.)
+   std::uint8_t storage_id;			        // Storage ID (1 for first, 2 for second, etc.)
    std::uint8_t storage_count;			    // Number of storage devices
    std::uint8_t status;			            // STORAGE_STATUS	Status of storage
-   std::float total_capacity;	                    // MiB	Total capacity. If storage is not ready (STORAGE_STATUS_READY) value will be ignored.
-   std::float used_capacity;		            // MiB	Used capacity. If storage is not ready (STORAGE_STATUS_READY) value will be ignored.
+   std::float total_capacity;	          // MiB	Total capacity. If storage is not ready (STORAGE_STATUS_READY) value will be ignored.
+   std::float used_capacity;		        // MiB	Used capacity. If storage is not ready (STORAGE_STATUS_READY) value will be ignored.
    std::float available_capacity;		    // MiB	Available storage capacity. If storage is not ready (STORAGE_STATUS_READY) value will be ignored.
-   std::float read_speed;	                    // MiB/s Read speed.
-   std::float write_speed;	                    // MiB/s Write speed.
-   std::uint8_t type; 	                            // STORAGE_TYPE	Type of storage
+   std::float read_speed;	              // MiB/s Read speed.
+   std::float write_speed;	            // MiB/s Write speed.
+   std::uint8_t type; 	                // STORAGE_TYPE	Type of storage
    std::char name[32]; 			            // Textual storage name to be used in UI (microSD 1, Internal Memory, etc.) This is a NULL terminated string. If it is exactly 32 characters long, add a terminating NULL. If this string is empty, the generic type is shown to the user.
-   std::uint8_t storage_usage; 	 	            // STORAGE_USAGE_FLAG	Flags indicating whether this instance is preferred storage for photos, videos, etc. Note: Implementations should initially set the flags on the system-default storage id used for saving media (if possible/supported). This setting can then be overridden using `MAV_CMD_SET_STORAGE_USAGE`. If the media usage flags are not set, a GCS may assume storage ID 1 is the default storage for all media types.
+   std::uint8_t storage_usage; 	 	      // STORAGE_USAGE_FLAG	Flags indicating whether this instance is preferred storage for photos, videos, etc. Note: Implementations should initially set the flags on the system-default storage id used for saving media (if possible/supported). This setting can then be overridden using `MAV_CMD_SET_STORAGE_USAGE`. If the media usage flags are not set, a GCS may assume storage ID 1 is the default storage for all media types.
    std::uint8_t reqfailure;
 };
 
@@ -173,6 +174,9 @@ enum mavCommandedState_e : std::int32_t
   SENT_CCS,
   SENS_VS,
   SEND_VS,
-  SENT_VS
+  SENT_VS,
+  SENS_FMT,
+  SEND_FMT,
+  SENT_FMT
 };
 #endif
