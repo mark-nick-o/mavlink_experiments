914fedcba1a7c66f706b1d052c6815db4d8512df
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-15 12:34:44 +0000

    Create send_camera_storage_info_handler.hpp

---
+++ uav/handlers/send_camera_storage_info_handler.hpp
@@ -0,0 +1,27 @@
+#ifndef SEND_CAMERA_STORAGE_INFO_HANDLER_H
+#define SEND_CAMERA_STORAGE_INFO_HANDLER_H
+
+#include "abstract_handler.h"
+
+namespace domain
+{
+    class UavModel;
+
+    class SendCameraStorageInfoHandler: public AbstractHandler
+    {
+    public:
+        SendCameraStorageInfoHandler(MavLinkCommunicator* communicator,
+                            UavModel* model);
+
+    public slots:
+        void processMessage(const mavlink_message_t& message) override;
+
+    protected:
+        void timerEvent(QTimerEvent *event) override;
+
+    private:
+        UavModel* m_model;
+    };
+}
+
+#endif // SEND_CAMERA_STORAGE_INFO_HANDLER_H
