e15667b4471c33f88ae856a81a663a3eeffaae6a
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-15 17:33:16 +0000

    Create send_camera_image_captured_handler.hpp

---
+++ uav/handlers/send_camera_image_captured_handler.hpp
@@ -0,0 +1,25 @@
+#ifndef SEND_CAM_IMG_CAP_HANDLER_H
+#define SEND_CAM_IMG_CAP_HANDLER_H
+
+#include "abstract_handler.h"
+
+namespace domain
+{
+    class UavModel;
+
+    class SendCameraImageCapturedHandler: public AbstractHandler
+    {
+    public:
+        SendCameraImageCapturedHandler(MavLinkCommunicator* communicator,
+                            UavModel* model);
+
+    public slots:
+        void processMessage(const mavlink_message_t& message) override;
+
+    protected:
+        void timerEvent(QTimerEvent *event) override;
+
+    private:
+        UavModel* m_model;
+    };
+}
