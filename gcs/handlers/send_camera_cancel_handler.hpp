0acf94fa91fd5992b71cad0322b0544ba03e10b1
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-12-16 10:36:48 +0000

    Create send_camera_cancel_handler.hpp

---
+++ gcs/handlers/send_camera_cancel_handler.hpp
@@ -0,0 +1,25 @@
+#ifndef SEND_CAM_CANCL_HANDLER_H
+#define SEND_CAM_CANCL_HANDLER_H
+
+#include "abstract_handler.h"
+
+namespace domain
+{
+
+    class GcsModel;
+    
+    class SendCameraCancelHandler: public SendCameraCancelHandler
+    {
+    public:
+        SendCameraCancelHandler(MavLinkCommunicator* communicator, GcsModel* model);
+
+    public slots:
+        void processMessage(const mavlink_message_t& message) override;
+
+    protected:
+        void timerEvent(QTimerEvent *event) override;
+
+    private:
+        GcsModel* m_model;
+    };
+}
