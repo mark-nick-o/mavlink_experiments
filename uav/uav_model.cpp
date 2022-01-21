292d686b23ede29b953c0959b7054ab713fa6080
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2022-01-21 17:06:32 +0000

    Update uav_model.cpp

--- uav/uav_model.cpp
+++ uav/uav_model.cpp
@@ -34,28 +34,27 @@ UavModel::UavModel(QObject* parent):
     m_roll(::randNum(-50, 50) * 0.1),
     m_yaw(::randNum(0, 360)),
     m_airspeed(::randNum(150, 500) * 0.1),
-    m_position(55.9837, 37.2088, 0)
-    m_time_boot_ms(65501)
-    m_firmware_version(1001)
-    m_focal_length(1.2)
-    m_sensor_size_h(1)
-    m_sensor_size_v(2)
-    m_flags(43)
-    m_resolution_h(4)
-    m_resolution_v(6)
-    m_cam_definition_version(8)
-    m_vendor_name("Sony")
-    m_model_name("alpha750")
-    m_lens_id(67)
-    m_cam_definition_uri("http://121.1.1.2/pics/1.jpg")
-    m_substate(0)
-    m_sendState(0)
-    m_reject(0)
-    m_ccs_update_trigger(0)
-    m_ccs_time_cycle(0)    
-    m_cic_interval(0)
-    m_ccs_time_cycle(0)     
-    m_disk1_count_of_images(0)
+    m_position(55.9837, 37.2088, 0),
+    m_time_boot_ms(65501),
+    m_firmware_version(1001),
+    m_focal_length(1.2),
+    m_sensor_size_h(1),
+    m_sensor_size_v(2),
+    m_flags(43),
+    m_resolution_h(4),
+    m_resolution_v(6),
+    m_cam_definition_version(8),
+    m_vendor_name("Sony"),
+    m_model_name("alpha750"),
+    m_lens_id(67),
+    m_cam_definition_uri("http://121.1.1.2/pics/1.jpg"),
+    m_substate(0),
+    m_sendState(0),
+    m_reject(0),
+    m_ccs_update_trigger(0),
+    m_ccs_time_cycle(0),    
+    m_cic_interval(0),
+    m_disk1_count_of_images(0),
     m_disk2_count_of_images(0)
        
 {
@@ -82,6 +81,8 @@ void UavModel::timerEvent(QTimerEvent* event)
     m_pitch = formatPitchRoll(m_pitch + ::randNum(-500, 500) * 0.01);
     m_roll = formatPitchRoll(m_roll + ::randNum(-500, 500) * 0.01);
     m_yaw = formatYaw(m_yaw + ::randNum(-250, 250) * 0.01);
+    
+    m_time_boot_ms = 655;
 }
 
 QGeoCoordinate UavModel::position() const
@@ -129,79 +130,77 @@ float UavModel::pitch() const
     return m_pitch;
 }
 
-std::uint32_t time_boot_ms() const
+std::uint32_t UavModel::time_boot_ms() const
 {
     return m_time_boot_ms; 
 }                                                    /*< [ms] Timestamp (time since system boot).*/
             
-std::uint32_t firmware_version() const
+std::uint32_t UavModel::firmware_version() const
 {
     return m_firmware_version;
 }
                                                      /*<  Version of the camera firmware (v << 24 & 0xff = Dev, v << 16 & 0xff = Patch, v << 8 & 0xff = Minor, v & 0xff = Major)*/
-float focal_length() const
+float UavModel::focal_length() const
 {
     return m_focal_length;    
 }
                                                        /*< [mm] Focal length*/
-float sensor_size_h() const
+float UavModel::sensor_size_h() const
 {
     return m_sensor_size_h;        
 }
                                                       /*< [mm] Image sensor size horizontal*/
-float sensor_size_v() const
+float UavModel::sensor_size_v() const
 {
     return m_sensor_size_v;        
 }
                                                       /*< [mm] Image sensor size vertical*/
-std::uint32_t flags() const
+std::uint32_t UavModel::flags() const
 {
     return m_flags;     
 }
                                                       /*<  Bitmap of camera capability flags.*/
-std::uint16_t resolution_h() const
+std::uint16_t UavModel::resolution_h() const
 {
     return m_resolution_h;        
 }
                                                       /*< [pix] Horizontal image resolution*/
-std::uint16_t resolution_v() const
+std::uint16_t UavModel::resolution_v() const
 {
     return m_resolution_v;   
 }
                                                      /*< [pix] Vertical image resolution*/
-std::uint16_t cam_definition_version() const
+std::uint16_t UavModel::cam_definition_version() const
 {
     return m_cam_definition_version;   
 }
                                                      /*<  Camera definition version (iteration)*/
-std::uint8_t* vendor_name() const
+std::uint8_t* UavModel::vendor_name() const
 {
-    return m_vendor_name;  
+    return const_cast<std::uint8_t*>(m_vendor_name);  
 }
                                                      /*<  Name of the camera vendor*/
-std::uint8_t* model_name() const
+std::uint8_t* UavModel::model_name() const
 {
-    return m_model_name;      
+    return const_cast<std::uint8_t*>(m_model_name);      
 }
                                                     /*<  Name of the camera model*/
-std::uint8_t lens_id() const
+std::uint8_t UavModel::lens_id() const
 {
     return m_lens_id;       
 }
                                                    /*<  Reserved for a lens ID*/
-std::char* cam_definition_uri() const
+std::uint8_t* UavModel::cam_definition_uri() const
 {
-    return m_cam_definition_uri;                  /*<  Camera definition URI (if any, otherwise only basic functions will be available). HTTP- (http://) and MAVLink FTP- (mavlinkftp://) formatted URIs are allowed (and both must be supported by any GCS that implements the Camera Protocol).*/
+    return const_cast<std::uint8_t*>(m_cam_definition_uri);                  /*<  Camera definition URI (if any, otherwise only basic functions will be available). HTTP- (http://) and MAVLink FTP- (mavlinkftp://) formatted URIs are allowed (and both must be supported by any GCS that implements the Camera Protocol).*/
 }
 
-std::int8_t get_substate() const
+std::int8_t UavModel::get_substate() const
 {
     return m_substate;       
 }
 
-std::int8_t get_sendState() const
+std::int8_t UavModel::get_sendState() const
 {
     return m_sendState;       
 }
-
-                                                   
