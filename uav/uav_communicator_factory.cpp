561ae366fb79d3689b7dbf7972f27a09f65e4a65
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2022-01-21 17:03:42 +0000

    Update uav_communicator_factory.cpp

--- uav/uav_communicator_factory.cpp
+++ uav/uav_communicator_factory.cpp
@@ -7,7 +7,7 @@
 #include "mavlink_communicator.h"
 
 #include "send_heartbeat_handler.hpp"
-#include "send_system_status_handler.h"
+//#include "send_system_status_handler.h"
 #include "send_position_handler.h"
 #include "send_attitude_handler.h"
 #include "send_vfr_hud_handler.h"
@@ -15,14 +15,14 @@
 #include "send_gps_raw_handler.h"
 #include "send_camera_info_handler.hpp"
 #include "send_camera_settings_handler.hpp"
-#include "send_camera_ack_handler.hpp"
-#include "send_camera_cancel_handler.hpp"
-#include "cmd_req_handler.hpp"
-#include "send_video_stream_handler.hpp"
-#include "send_battery_status.hpp"
-#include "send_camera_storage_info_handler.hpp"
-#include "send_camera_capture_status_handler.hpp"
-#include "send_camera_image_captured_handler.hpp"
+//#include "send_camera_ack_handler.hpp"
+//#include "send_camera_cancel_handler.hpp"
+//#include "cmd_req_handler.hpp"
+//#include "send_video_stream_handler.hpp"
+//#include "send_battery_status.hpp"
+//#include "send_camera_storage_info_handler.hpp"
+//#include "send_camera_capture_status_handler.hpp"
+//#include "send_camera_image_captured_handler.hpp"
 
 using namespace domain;
 
@@ -35,7 +35,7 @@ MavLinkCommunicator* UavCommunicatorFactory::create()
     MavLinkCommunicator* communicator = new MavLinkCommunicator(1, 0);
    
     new domain::HeartbeatHandler(MAV_TYPE_FIXED_WING, communicator);
-    new domain::CmdReqHandler(communicator, m_model); 
+    //new domain::CmdReqHandler(communicator, m_model); 
     
     new domain::SendSystemStatusHandler(communicator, m_model);
     new domain::SendPositionHandler(communicator, m_model);
@@ -44,14 +44,14 @@ MavLinkCommunicator* UavCommunicatorFactory::create()
     new domain::SendHomePositionHandler(communicator, m_model);
     new domain::SendGpsRawHandler(communicator, m_model);
     new domain::SendCameraInfoHandler(communicator, m_model);
-    new domain::SendCameraAckHandler(communicator, m_model);
-    new domain::SendCameraCancelHandler(communicator, m_model);
+    //new domain::SendCameraAckHandler(communicator, m_model);
+    //new domain::SendCameraCancelHandler(communicator, m_model);
     new domain::SendCameraSettingsHandler(communicator, m_model);
-    new domain::SendVideoStreamInfoHandler(communicator, m_model);
-    new domain::SendBatterStatusHandler(communicator, m_model);
-    new domain::SendCameraStorageInfoHandler(communicator, m_model);
-    new domain::SendCameraCaptureStatusHandler(communicator, m_model);
-    new domain::SendCameraImageCapturedHandler(communicator, m_model);
+    //new domain::SendVideoStreamInfoHandler(communicator, m_model);
+    //new domain::SendBatterStatusHandler(communicator, m_model);
+    //new domain::SendCameraStorageInfoHandler(communicator, m_model);
+    //new domain::SendCameraCaptureStatusHandler(communicator, m_model);
+    //new domain::SendCameraImageCapturedHandler(communicator, m_model);
     
     return communicator;
 }
