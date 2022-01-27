55a8ce417d03ed7fda089ce8ccb2f77d9bcdd8a1
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2022-01-27 23:59:44 +0000

    Update web.py

--- web.py
+++ web.py
@@ -21,20 +21,34 @@ import numpy as np
 #
 import json
 from flask import Flask, render_template
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 #
 # https://habr.com/ru/post/472126/ ref client server / alternative
 #
+   
+@app.route('/take_a_photo')
+def take_a_photo_handler():
+    #if camera.ready:
+    #res = saveOnePicture()
+    res="/home/pi/cams/src/acp/sonyCam"
+    return render_template('one_photo.html', photo_path=res, user="shithead")
+    #return render_template('one_photo.html')
+    #else:
+    #    return "Failure to take picture"
 
-#
-# route to the xml file must be held in subdirectory templates
-#
 @app.route('/cam_defs')
 def send_cam_defs_handler():
     return render_template('alpha_cam_new.xml')
 
+
+@app.route('/cam_default')
+def send_cam_default_handler():
+    return render_template('cam_default.xml')
+
 if __name__ == '__main__':
 
     # run flask server
-    app.run(host='0.0.0.0', port=80,debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True) 
+    
