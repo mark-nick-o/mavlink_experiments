0897cc2af5a86a9373b83d91a9a2eac68bd2ccdc
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2022-02-14 18:14:05 +0000

    Update start_mav_router.sh

--- start_mav_router.sh
+++ start_mav_router.sh
@@ -4,8 +4,10 @@
 #
 # ============================================================================================
 #
-ps -ael | grep mavlink-routerd > /dev/null
+MAVROUTEHOME=/home/pi/mav/mav_route/mavlink-router/build
+ps -ael | grep mavlink-routerd > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-    /usr/bin/mavlink-routerd  -e 10.0.2.51:14550 /dev/ttyACM0:115200 > /home/pi/mav/mav_route/mavlink-router/build/mavrd.log &
+    sudo $MAVROUTEHOME/kill_all_mavroute_ports.sh > /dev/null 2>&1
+    /usr/bin/mavlink-routerd  -e 10.0.2.51:14550 -e 127.0.0.1:14551 /dev/ttyACM0:115200 > /home/pi/mav/mav_route/mavlink-router/build/mavrd.log 2>&1 &
 fi
